5f24cbb5a818b8abc1fc9fedcbd05dce04fb9ba9
lokeshbs1994 <lokeshbs619@gmail.com>
2022-03-19 11:56:34 +0530

    ADDED: Adding Hotels with Rates for regular customers

---
+++ src/test/java/com/bridgelabz/HotelReservationSystemTest.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class HotelReservationSystemTest {
+    @Test
+    public void givenHotelDetails_WhenValuesEnteredAreCorrect_ShouldReturnTrue() {
+        HotelReservation hotelReservation = new HotelReservation();
+        boolean isValidHotel1 = hotelReservation.addHotel("Hotel_1", "Lakewood", 110, 90);
+        Assert.assertEquals(true, isValidHotel1);
+        boolean isValidHotel2 = hotelReservation.addHotel("Hotel_2", "Bridgewood", 160, 60);
+        Assert.assertEquals(true, isValidHotel2);
+        boolean isValidHotel3 = hotelReservation.addHotel("Hotel_3", "Ridgewood", 220, 150);
+        Assert.assertEquals(true, isValidHotel3);
+        hotelReservation.displayHotels();
+    }
+}
